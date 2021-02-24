66d8ac37d0760a3ac0842db37d353cf061d94f77
alexisrw33 <69729270+alexisrw33@users.noreply.github.com>
2021-02-23 19:17:54 -0600

    added tab bar, collection view and set up, and superhero api network request

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Alexis Rojas-Westall on 2/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
